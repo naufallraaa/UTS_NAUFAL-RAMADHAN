acbd175730fe5c7c30dc3bec53d26f8291d50c5a
Naufal Ramadhan <naufalramadhang.com>
2024-05-04 18:27:27 +0700

    commit pertama

---
+++ src/views/uts/home.vue
@@ -0,0 +1,9 @@
+<template>
+<h1>SELAMAT DATANG DI APLIKASI</h1>
+</br>
+<h2>Aplikasi ini dibuat oleh : </h2>
+</br>
+<h3>Nama : Naufal Ramadhan</h3>
+</br>
+<h3>NPM : 4062210056</h3><br>
+</template>
